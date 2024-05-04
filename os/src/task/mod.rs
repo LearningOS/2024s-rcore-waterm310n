b9977de5857afd3696316d06ecdeb75a65653f26
wat3rmelon <wat3rmelon2000@gmail.com>
2024-05-04 17:48:32 +0800

    合并munmap，并通过修改mmap的参数检查修复了mmap

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -33,7 +33,7 @@ pub use id::{kstack_alloc, pid_alloc, KernelStack, PidHandle};
 pub use manager::add_task;
 pub use processor::{
     current_task, current_trap_cx, current_user_token, run_tasks, schedule, take_current_task,
-    Processor,get_task_info,update_task_syscall_num,mmap_helper
+    Processor,get_task_info,update_task_syscall_num,mmap_helper,munmap_helper
 };
 
 
@@ -117,3 +117,4 @@ lazy_static! {
 pub fn add_initproc() {
     add_task(INITPROC.clone());
 }
+
