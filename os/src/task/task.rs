cb89ba971715504aa21aff4ea4d3eba72e8edf4f
wat3rmelon <wat3rmelon2000@gmail.com>
2024-05-06 20:40:39 +0800

    复制fork与exec的代码，减去不必要的地址空间创建，就通过测试样例了。。

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -249,6 +249,59 @@ impl TaskControlBlock {
             None
         }
     }
+
+    /// 直接创建使用elf数据创建子进程，不复制地址空间
+    pub fn spawn(self:&Arc<Self>,elf_data: &[u8]) -> Arc<Self> {
+        // ---- access parent PCB exclusively
+        let mut parent_inner = self.inner_exclusive_access();
+        // copy user space(include trap context)
+        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
+        let trap_cx_ppn = memory_set
+            .translate(VirtAddr::from(TRAP_CONTEXT_BASE).into())
+            .unwrap()
+            .ppn();
+    
+        // alloc a pid and a kernel stack in kernel space
+        let pid_handle = pid_alloc();
+        let kernel_stack = kstack_alloc();
+        let kernel_stack_top = kernel_stack.get_top();
+        let task_control_block = Arc::new(TaskControlBlock {
+            pid: pid_handle,
+            kernel_stack,
+            inner: unsafe {
+                UPSafeCell::new(TaskControlBlockInner {
+                    trap_cx_ppn,
+                    base_size: user_sp,
+                    task_cx: TaskContext::goto_trap_return(kernel_stack_top),
+                    task_status: TaskStatus::Ready,
+                    memory_set,
+                    parent: Some(Arc::downgrade(self)),
+                    children: Vec::new(),
+                    exit_code: 0,
+                    heap_bottom: user_sp,
+                    program_brk: user_sp,
+                    syscall_nums:[0; MAX_SYSCALL_NUM],
+                    has_been_run:false,
+                    first_run_time:0,
+                })
+            },
+        });
+        // add child
+        parent_inner.children.push(task_control_block.clone());
+        // prepare TrapContext in user space
+        let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
+        *trap_cx = TrapContext::app_init_context(
+            entry_point,
+            user_sp,
+            KERNEL_SPACE.exclusive_access().token(),
+            kernel_stack_top,
+            trap_handler as usize,
+        );
+        // return
+        task_control_block
+        // **** release child PCB
+        // ---- release parent PCB
+    }
 }
 
 #[derive(Copy, Clone, PartialEq)]
